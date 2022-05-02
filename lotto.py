bdf17db5b09784c27e29e91649acc23b9192aa91
Jinsoo Kim <jskim8624@naver.com>
2022-05-02 22:34:16 +0900

    랜덤 번호를 통한 번호 추출

--- lotto.py
+++ lotto.py
@@ -1,6 +1,6 @@
 import os
 import cx_Oracle
-
+import random
 def myCon():
     dsn = cx_Oracle.makedsn("systrade.iptime.org", 1521, service_name = "XE") # 오라클 주소
     connection = cx_Oracle.connect(user="indisystrade", password="sys123", dsn=dsn, encoding="UTF-8") # 오라클 접속
@@ -32,7 +32,7 @@ def test03(connection):
 
 def test04(connection):
     cur = connection.cursor()
-    cur.execute("select * from lotto order by seq desc")
+    cur.execute("select * from (select * from lotto order by seq desc) A where rownum <=5")
     rows = cur.fetchall()   # 리턴한 객체를 한번에 리턴시킨다.
     for row in rows:
         print(row)
@@ -52,6 +52,7 @@ def insert( connection, seq, ball1, ball2, ball3, ball4, ball5, ball6, ball_bonu
         print(error)
 
 
+
 LOCATION = r"C:\Users\jskim\PycharmProjects\instantclient_21_3"
 os.environ["PATH"] = LOCATION + ";" + os.environ["PATH"] #환경변수 등록
 
@@ -68,9 +69,25 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #insert(myCon(),1012, 5, 11, 18, 20, 35, 45, 3)
-    test04(myCon())
-    print("=============================#########################################3")
+    #insert(myCon(),1013, 21, 22, 26, 34, 36, 41, 32)
+    #test04(myCon())
+    #print("=============================#########################################3")
+    arr = [21, 22, 26, 34, 36, 41, 32,
+           5, 11, 18, 20, 35, 45, 3,
+           1, 9, 12, 26, 35, 38, 42,
+           9, 12, 15, 25, 34, 36, 3,
+           15, 23, 29, 34, 40, 44, 20]
+    for i in range(0, len(arr)):
+        print(i, arr[i])
+
+    for j in range(1, 6):
+        random_number = random.randint(0, len(arr) - 1)
+        ball1 = arr[random_number]
+        random_number = random.randint(0, len(arr) - 1)
+        ball2 = arr[random_number]
+        random_number = random.randint(0, len(arr) - 1)
+        ball3 = arr[random_number]
+        print(j, ball1, ball2, ball3)
     #print_hi(os.environ["PATH"])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
